aff843259e918bf62fdaedfb52a184231749d1a7
CanaanGM <reakak1@gmail.com>
2023-03-14 14:43:10 +0300

    made it a list and play will choose one at random

--- lofi-music.py
+++ lofi-music.py
@@ -1,8 +1,13 @@
 import argparse
 import webbrowser
+from random import choice
 
 # LOFI_MUSIC_URL = 'https://www.youtube.com/watch?v=5qap5aO4i9A'
-LOFI_MUSIC_URL = 'https://www.youtube.com/watch?v=jfKfPfyJRdk'
+LOFI_MUSIC_URLS: list[str] = ['https://www.youtube.com/watch?v=jfKfPfyJRdk',
+                   "https://www.youtube.com/watch?v=GqnYz3Ty3Qc",
+                   "https://www.youtube.com/watch?v=mxH_9dlUntU",
+                   "https://www.youtube.com/watch?v=KJFWkgrwxJQ"
+                   ]
 
 
 def main():
@@ -12,7 +17,7 @@ def main():
     args = parser.parse_args()
 
     if args.play:
-        webbrowser.open(LOFI_MUSIC_URL, new=1)
+        webbrowser.open(choice(LOFI_MUSIC_URLS), new=1)
 
     if args.stop:
         for handle in webbrowser._tryorder:
