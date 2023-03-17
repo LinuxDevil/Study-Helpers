e46edc74d454e24cbde48f17960b85d80e03af7b
Ali Mohammad <alimohammad@pc-2473.home>
2023-03-17 17:45:34 +0300

    fix: light house automation

--- site-lighthouse.py
+++ site-lighthouse.py
@@ -1,60 +1,49 @@
+import subprocess
+import json
 import argparse
-import requests
-from bs4 import BeautifulSoup
-from lighthouse import lighthouse
+import datetime
 
-class Test:
-    def __init__(self, url):
-        self.url = url
-    
-    def run(self):
-        self._run_seo_tests()
-        self._run_performance_tests()
-        self._run_best_practices_tests()
-        self._run_accessibility_tests()
 
-    def _run_seo_tests(self):
-        response = requests.get(self.url)
-        html = response.text
-
-        soup = BeautifulSoup(html, 'html.parser')
-
-        title = soup.title.text
-        if len(title) < 50:
-            print('Title tag is too short: {}'.format(title))
-
-        meta_desc = soup.find('meta', attrs={'name': 'description'})
-        if not meta_desc or len(meta_desc.get('content', '')) < 120:
-            print('Meta description is too short or missing')
-
-        h1_tags = soup.find_all('h1')
-        if len(h1_tags) == 0:
-            print('No H1 tags found')
-
-        img_tags = soup.find_all('img')
-        if len(img_tags) == 0:
-            print('No images found')
-
-    def _run_performance_tests(self):
-        results = lighthouse(self.url, config_path={'extends': 'lighthouse:default', 'settings': {'emulatedFormFactor': 'desktop'}},
-                              flags={'onlyCategories': ['performance']})
-        print('Performance score:', results['categories']['performance']['score'])
-
-    def _run_best_practices_tests(self):
-        results = lighthouse(self.url, config_path={'extends': 'lighthouse:default', 'settings': {'emulatedFormFactor': 'desktop'}},
-                              flags={'onlyCategories': ['best-practices']})
-        print('Best practices score:', results['categories']['best-practices']['score'])
-
-    def _run_accessibility_tests(self):
-        results = lighthouse(self.url, config_path={'extends': 'lighthouse:default', 'settings': {'emulatedFormFactor': 'desktop'}},
-                              flags={'onlyCategories': ['accessibility']})
-        print('Accessibility score:', results['categories']['accessibility']['score'])
+def run_lighthouse(url, flags):
+    """Runs Lighthouse audit on the given URL with the given flags"""
+    lighthouse_command = f'lighthouse {url} --output=json --quiet {" ".join(flags)}'
+    process = subprocess.Popen(lighthouse_command, stdout=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+    if error:
+        raise Exception(error)
+    return json.loads(output)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Run SEO, performance, best practices, and accessibility tests on a website.')
-    parser.add_argument('url', help='URL of website to test')
+    parser = argparse.ArgumentParser(description='Run Google Lighthouse as a CLI')
+    parser.add_argument('url', help='URL to run Lighthouse audit on')
+    parser.add_argument('-p', '--performance', action='store_true', help='Include performance audit')
+    parser.add_argument('-a', '--accessibility', action='store_true', help='Include accessibility audit')
+    parser.add_argument('-b', '--best-practices', action='store_true', help='Include best practices audit')
+    parser.add_argument('-s', '--seo', action='store_true', help='Include SEO audit')
     args = parser.parse_args()
 
-    test = Test(args.url)
-    test.run()
+    flags = []
+    if args.performance:
+        flags.append('--only-categories=performance')
+    if args.accessibility:
+        flags.append('--only-categories=accessibility')
+    if args.best_practices:
+        flags.append('--only-categories=best-practices')
+    if args.seo:
+        flags.append('--only-categories=seo')
+
+    # Get the current date and time
+    now = datetime.datetime.now()
+    timestamp = now.strftime('%Y-%m-%d')
+
+    # Extract the website name from the URL
+    website_name = args.url.split('//')[1].split('/')[0]
+
+    # Run Lighthouse audit and save the report
+    result = run_lighthouse(args.url, flags)
+    report_name = f'{website_name}-{timestamp}.json'
+    with open(report_name, 'w') as f:
+        f.write(json.dumps(result, indent=4))
+
+    print(f'Lighthouse report saved as {report_name}')
