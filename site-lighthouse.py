36a3c30840fdabd226283cbaefb6968174905007
Ali Mohammad <alimohammad@pc-2473.home>
2023-03-17 17:54:44 +0300

    chore: refactor the site-house to avoid mixing abstractions

--- site-lighthouse.py
+++ site-lighthouse.py
@@ -4,6 +4,29 @@ import argparse
 import datetime
 
 
+def get_cli_flags(args) -> [str]:
+    cli_flags = []
+    if args.performance:
+        cli_flags.append('--only-categories=performance')
+    if args.accessibility:
+        cli_flags.append('--only-categories=accessibility')
+    if args.best_practices:
+        cli_flags.append('--only-categories=best-practices')
+    if args.seo:
+        cli_flags.append('--only-categories=seo')
+    return cli_flags
+
+
+def get_cli_args():
+    parser = argparse.ArgumentParser(description='Run Google Lighthouse as a CLI')
+    parser.add_argument('url', help='URL to run Lighthouse audit on')
+    parser.add_argument('-p', '--performance', action='store_true', help='Include performance audit')
+    parser.add_argument('-a', '--accessibility', action='store_true', help='Include accessibility audit')
+    parser.add_argument('-b', '--best-practices', action='store_true', help='Include best practices audit')
+    parser.add_argument('-s', '--seo', action='store_true', help='Include SEO audit')
+    return parser.parse_args()
+
+
 def run_lighthouse(url, flags):
     """Runs Lighthouse audit on the given URL with the given flags"""
     lighthouse_command = f'lighthouse {url} --output=json --quiet {" ".join(flags)}'
@@ -14,36 +37,24 @@ def run_lighthouse(url, flags):
     return json.loads(output)
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Run Google Lighthouse as a CLI')
-    parser.add_argument('url', help='URL to run Lighthouse audit on')
-    parser.add_argument('-p', '--performance', action='store_true', help='Include performance audit')
-    parser.add_argument('-a', '--accessibility', action='store_true', help='Include accessibility audit')
-    parser.add_argument('-b', '--best-practices', action='store_true', help='Include best practices audit')
-    parser.add_argument('-s', '--seo', action='store_true', help='Include SEO audit')
-    args = parser.parse_args()
-
-    flags = []
-    if args.performance:
-        flags.append('--only-categories=performance')
-    if args.accessibility:
-        flags.append('--only-categories=accessibility')
-    if args.best_practices:
-        flags.append('--only-categories=best-practices')
-    if args.seo:
-        flags.append('--only-categories=seo')
-
-    # Get the current date and time
+def save_report(args, result):
     now = datetime.datetime.now()
     timestamp = now.strftime('%Y-%m-%d')
-
-    # Extract the website name from the URL
     website_name = args.url.split('//')[1].split('/')[0]
 
-    # Run Lighthouse audit and save the report
-    result = run_lighthouse(args.url, flags)
     report_name = f'{website_name}-{timestamp}.json'
     with open(report_name, 'w') as f:
         f.write(json.dumps(result, indent=4))
 
     print(f'Lighthouse report saved as {report_name}')
+
+
+if __name__ == '__main__':
+    _args = get_cli_args()
+    _flags = get_cli_flags(_args)
+
+    # Run Lighthouse audit and save the report
+    _result = run_lighthouse(_args.url, _flags)
+    print('Light house finished! Saving Report...')
+    save_report(_args, _result)
+
